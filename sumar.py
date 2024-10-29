7a471ef657ad819274bc79ab3c185b5650e76b00
agus-romero96 <jansen.rm96@gmail.com>
2024-10-29 08:53:04 -0500

    Update sumar.py

--- sumar.py
+++ sumar.py
@@ -2,7 +2,7 @@ x= 6
 y=10
 z=x+y
 print("la suma es:",z)
-if z>10:
-        print("La suma es mayor a 10")
+if (z>10):
+    print("La suma es mayor a 10")
 else:
-  print("la suma es otro valor")
+    print("la suma es menor a 10")
